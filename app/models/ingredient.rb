0497f55a82df791d51f57fe68988633c7ca9aa7a
Aniket <aniketupatel1@gmail.com>
2020-01-16 14:52:50 -0500

    added associations to models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+    has_many :meal_ingredients
+    has_many :meals, through: :meal_ingredients
+    has_many :ingredient_nutrients
+    has_many :nutrients, through: :ingredient_nutrients
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+    has_many :user_meals
+    has_many :meals, through: :user_meals
     has_secure_password 
 end
