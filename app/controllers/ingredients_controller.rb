fc21d50491bc0043a97cff8471370d6a7db29538
Aniket <aniketupatel1@gmail.com>
2020-01-25 15:46:53 -0500

    added ingredients controller with routes

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,48 @@
+class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :update, :destroy]
+
+  def index
+    @ingredients = Ingredient.all
+
+    render json: @ingredients
+  end
+
+  def show
+    render json: @ingredient
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+
+    if @ingredient.save
+      render json: @ingredient, status: :created, location: @ingredient
+    else
+      render json: @ingredient.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @ingredient.update(ingredient_params)
+      render json: @ingredient
+    else
+      render json: @ingredient.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @ingredient.destroy
+  end
+
+  private 
+
+  def set_ingredient
+      @ingredient = Ingredient.find(params[:id])
+    end
+
+    
+    def ingredient_params
+      params.require(:ingredient).permit(:name, :food_group, :qty, :unit, :weight, :photo, :calories)
+    end
+
+
+end
